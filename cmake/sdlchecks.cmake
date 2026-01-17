f54ef311b60cb4919cc9e672964fc4221f598983
KAI4MAN <51535323+kai4man@users.noreply.github.com>
2026-01-18 02:21:18 +0500

    Refactor CheckMali macro for SDL_MALI support

--- cmake/sdlchecks.cmake
+++ cmake/sdlchecks.cmake
@@ -836,25 +836,24 @@ endmacro()
 # Requires:
 # - n/a
 macro(CheckMali)
-  if(VIDEO_MALI)
-    set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
+  if(SDL_MALI)
     check_c_source_compiles("
         #define LINUX
         #define EGL_API_FB
         #include <EGL/egl.h>
         int main(int argc, char** argv) {}" HAVE_VIDEO_MALI_EGL_FB)
     if(HAVE_VIDEO_MALI_EGL_FB)
-      set(HAVE_VIDEO_MALI TRUE)
+      set(HAVE_MALI TRUE)
       set(HAVE_SDL_VIDEO TRUE)
+      set(SDL_VIDEO_DRIVER_MALI 1)
 
       file(GLOB MALI_SOURCES ${SDL2_SOURCE_DIR}/src/video/mali-fbdev/*.c)
-      set(SOURCE_FILES ${SOURCE_FILES} ${MALI_SOURCES})
-      set(SDL_VIDEO_DRIVER_MALI 1)
-      set(SDL_CFLAGS "${SDL_CFLAGS} -DLINUX -DEGL_API_FB")
+      list(APPEND SOURCE_FILES  ${MALI_SOURCES})
+      list(APPEND SDL_CFLAGS -DLINUX -DEGL_API_FB)
       list(APPEND EXTRA_LIBS EGL)
-    endif(HAVE_VIDEO_MALI_EGL_MALI)
-  endif(VIDEO_MALI)
-endmacro(CheckMali)
+    endif()
+  endif()
+endmacro()
 
 # Requires:
 # - nada
